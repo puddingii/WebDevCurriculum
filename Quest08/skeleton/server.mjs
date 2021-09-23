045383b2d75909c4c3d9a333c87228346f0dae1d
GeonBbang <gun4930@naver.com>
2021-09-23 15:31:43 +0900

    GET,POST를 이용하여 문자열처리 완 + file관리시스템 진행중

--- Quest08/skeleton/server.mjs
+++ Quest08/skeleton/server.mjs
@@ -1,8 +1,67 @@
 import http from 'http';
+import qs from "querystring";
+import fs from "fs";
+import { Buffer } from "buffer";
 
+const ROOT = "http://localhost:8080";
 const server = http.createServer((req, res) => {
-    /* TODO: 각각의 URL들을 어떻게 처리하면 좋을까요? */
-    res.end();
+    const myUrl = new URL(ROOT+req.url);
+    if('/' === myUrl.pathname) {
+        res.end("home");
+    }
+    if('/foo' === myUrl.pathname) {
+        if(req.method === "GET") {
+            res.end(`Hello, ${myUrl.searchParams.get("bar")}`);
+        } else if(req.method === "POST") {
+            req.setEncoding('utf-8');
+            let resData = '';
+
+            req.on('data', (chunk) => {
+                resData += chunk;
+                resData = qs.parse(resData);
+            });
+            
+            req.on('end', () => {
+                const { bar } = resData;
+                res.end(`Hello, ${bar}`);
+            })
+        }
+    }
+
+    const picUrl = myUrl.pathname.split('/');
+    if(picUrl[1] === "pic") {
+        if(picUrl[2] === "upload") {
+            let fileSrc;
+            req.on('data', (chunk) => {
+                console.log(chunk);
+                fileSrc = chunk; //new Buffer.from(chunk).toString('base64');
+                console.log(fileSrc);
+            })
+            req.on('end', () => {
+                fs.writeFile("test.jpg", fileSrc, (err) => {
+                    console.log(err);
+                });
+
+            })
+        }
+    }
+
+    console.log(req.method);
 });
+
+const data = JSON.stringify({
+    "msg": 'postimmm'
+})
+
+const options = {
+    hostname: "localhost",
+    port: 8000,
+    path: '/foo',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': Buffer.byteLength(`Hello, ${data}`)
+    }
+};
 
 server.listen(8000);
